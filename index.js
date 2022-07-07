49fe692380c0801c2baf9cea7b1454764f7cbe31
Alex Mancinas <alexman381@gmail.com>
2022-07-07 14:48:13 -0700

    part 2 whiout finish

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
-const API_URL = 'https://restcountries.com/v3.1';
+document.addEventListener('DOMContentLoaded', initialize)
 
-fetch(`${API_URL}/all`).then((res)=> res.json()).then((countries)=> {
-    const tpl = countries.map(country => `<li>${country.name.common}</li>`)
-  
-    console.log(tpl)
-})
+const API_URL = 'https://restcountries.com/v3.1/all';
+
+let content = document.querySelector('#table')
+let countries;
+
+var oficial_name
+
+fetch(API_URL)
+    .then((res) => res.json()).then(data => initialize(data)).catch((err) => console.log(err))
+
+function initialize(countriesData) {
+     countries =countriesData  
+    for (let i = 0; i <= countries.length; i++){
+        console.log('Capital of ' + countries[i].name.official + " is " + countries[i].capital)   
+    }
+    for(let i = 0; i<= countriesData.length;i++){
+           
+        var oficial_name=countries[i].name.official
+        var capital= countries[i].capital?countries[i].capital:"No capital"
+        var region= countries[i].region
+        var language= countries[i].languages?countries[i].languages:"No languages"
+        var population =countries[i].population
+        var flag =countries[i].flag
+
+        
+    }
+   
+
+    console.log(oficial_name)
+    }
+
+
